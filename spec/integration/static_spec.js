94c1d345003c2d4d33fdd45b77422468d1733937
Renuka <renukaalurkar@gmail.com>
2019-11-02 23:21:12 -0500

    first checkpoint

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,23 @@
+const request = require("request"); // import the request module  which we will use to make requests to the server during testing
+const server = require("../../src/server");
+const base = "http://localhost:3000/";//base url that we will ue for our requests
+
+describe("routes : static", () => {
+
+//#1
+  describe("GET /", () => {
+
+//#2
+    it("should return status code 200", (done) => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+//#4
+        done();
+      });
+    });
+
+  });
+});
