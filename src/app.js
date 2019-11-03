996ee9719119259d41cde240211396397c9f58f6
Renuka <renukaalurkar@gmail.com>
2019-11-03 07:24:50 -0600

    resolving git errors

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
 
- routeConfig.init(app);
+routeConfig.init(app);
 module.exports = app;
