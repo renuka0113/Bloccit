5fffa1ef3fe9d6f8920d2234b7ff9265bbbb9d37
Renuka <renukaalurkar@gmail.com>
2019-11-04 09:09:40 -0600

    resolving error

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -23,7 +23,7 @@ describe("routes : static", () => {
 
   describe("GET /marco",() => {
     it("should return status code 200",(done) => {
-      request.get(base,(err, res, body) => {
+      request.get(`${base}marco`,(err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(res.body).toBe("polo");
       })
