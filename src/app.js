3c60081ade3ee7d1f756dab0c600bd2fba0974e7
Renuka <renukaalurkar@gmail.com>
2019-11-06 11:48:53 -0600

    checkpoint completed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 const routeConfig = require("./config/route-config.js");
 const appConfig = require("./config/main-config.js")
 
-appConfig.init();
+//appConfig.init();
+appConfig.init(app, express);
 routeConfig.init(app);
 module.exports = app;
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,6 @@
 module.exports = {
   index(req, res, next){   //index is a function that will contain the route handler for the / route in src/routes/static.js
-    res.send("Welcome to Bloccit");
+    //res.send("Welcome to Bloccit");
+    res.render("static/index", {title: "Welcome to Bloccit"}); //render method takes the location of the template and an object containing the data we wish to make available in the template.<%= title %> will now return "Welcome to Bloccit."
   }
 }
