30bdf8d281f254bd5bdd83f49361a8066cce62fd
Renuka <renukaalurkar@gmail.com>
2019-11-08 08:50:49 -0600

    working on assignment

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,6 +1,9 @@
 module.exports = {
   index(req, res, next){   //index is a function that will contain the route handler for the / route in src/routes/static.js
     //res.send("Welcome to Bloccit");
-    res.render("static/index", {title: "Welcome to Bloccit"}); //render method takes the location of the template and an object containing the data we wish to make available in the template.<%= title %> will now return "Welcome to Bloccit."
+    res.render("static/index", {title: "Welcome to Bloccit"});
+    //render method takes the location of the template and an object containing the data we wish to make available in the template.The second argument means that make available the variable called title and set its value to "Welcome to Bloccit"
+    //index is index.ejs.   head.ejs contains %title% and we have included head.ejs in index.ejs
+    res.render("static/index",)
   }
 }
