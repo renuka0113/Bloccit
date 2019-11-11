e74dfc396e15e358a11b2483e020d8f1912b62e5
Renuka <renukaalurkar@gmail.com>
2019-11-11 11:01:40 -0600

    created model Rule

---
+++ src/db/models/rule.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Rule = sequelize.define('Rule', {
+    description: DataTypes.STRING
+  }, {});
+  Rule.associate = function(models) {
+    // associations can be defined here
+  };
+  return Rule;
+};
