3a8f4fb9db32c7854f32efd93b03f20be2b3f719
Renuka <renukaalurkar@gmail.com>
2019-11-10 22:44:09 -0600

    until before topic model

--- src/routes/static.js
+++ src/routes/static.js
@@ -10,7 +10,8 @@ const staticController = require("../controllers/staticController");//importing 
 router.get("/", staticController.index);
 
 router.get("/marco",(req, res, next) => {
-  res.send("polo");
-});
+  res.send("polo"); //what we are doing for the route "marco" is the same as what we did for the routes "/" or "/about" or "/topics".
+});//For "/","/about","/topics" routes, the (req,res,next) part is in their respective controller files.For example, for "/" route it isin staticController.index
+//For "/topics" route it is in topicController.js and same for "/about" route, this part of (req,res,next) is in staticController,topic function.
 
 module.exports = router;
