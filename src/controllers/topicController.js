16087f4d0beebc4461f2bac76c77188391111bba
Renuka <renukaalurkar@gmail.com>
2019-11-13 19:04:00 -0600

    corrected topicController

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -3,12 +3,13 @@ module.exports = {
   index(req, res, next){
   //  res.send("TODO: list all topics");
   topicQueries.getAllTopics((err, topics) => {
-
+  console.log(err);
 //#3
         if(err){
           res.redirect(500, "static/index");
         } else {
-          res.render("topics/index", {topics});
+          //res.render("topics/index", {topics});
+          res.render("topics/index", { topics, title: "Topics" });
         }
       })
   }
