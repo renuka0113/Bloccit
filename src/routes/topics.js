0a384b5baef113284f85ece758bb75ec760cfc38
Renuka <renukaalurkar@gmail.com>
2019-11-19 22:16:02 -0600

    until before show,destroy

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -5,5 +5,6 @@ const topicController = require("../controllers/topicController")
 
 router.get("/topics", topicController.index);
 router.get("/topics/new", topicController.new);
+router.post("/topics/create", topicController.create);
 
 module.exports = router;
