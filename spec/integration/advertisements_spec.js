088a4d13915fe9e39744456e4006d3e3e8b9ce0c
Renuka <renukaalurkar@gmail.com>
2019-11-22 09:13:45 -0600

    completed until advertisement migrations successful

--- spec/integration/advertisements_spec.js
+++ spec/integration/advertisements_spec.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000/topics/";
+const base = "http://localhost:3000/advertisements/";
 
 describe("routes : advertisements", () => {
 
---
+++ src/controllers/advertisementController.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index(req, res, next){     //creating a index function
+    res.send("TODO: list all topics");
+  }
+}
