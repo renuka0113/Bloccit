8a767291ef6182c16479defe841a9bedde4193e9
Renuka <renukaalurkar@gmail.com>
2019-11-23 22:05:48 -0600

    working on assignment

---
+++ src/db/queries.advertisements.js
@@ -0,0 +1,17 @@
+const Advertisement = require("./models").Advertisement; //requiring the Advertisement model that we created using sequelize mode:create etc in a constant variable Advertisement
+
+module.exports = {
+
+//#1
+  getAllAdvertisements(callback){  //we define a function getAllAdvertisement with callback as a argument
+    return Advertisement.findAll()  //we are calling the findAll method on the Advertisement model, this will return all the records in the Advertisement table.
+
+//#2
+    .then((advertisements) => {
+      callback(null, advertisements);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+}
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -3,14 +3,18 @@ const Topic = require("./models").Topic;
 module.exports = {
 
 //#1
-  getAllTopics(callback){
-    return Topic.findAll()
+//Wherever in the code the getAllTopics function is called, its called with a parameter that is passed to the getAllTopics function in the form of callback.
+//callback here is not a keyword, you can give it any name,we are calling it here as callback.
+//the callback parameter is a function that again gets called if the return part of the getALlTopics is successful.
+//For instance, if we call getAllTopics somewhere in the code, it is called as getAllTopics(myfunc2), this myFunc2 is pased as parameter callback here.
+  getAllTopics(callback){ //we define a getAllTopics function with a parameter callback. The getAllTopics is a function that returns the promise Topic.findAll
+    return Topic.findAll() //.findAll is a method on the table Topic and will return all the records of the Topic table
 
 //#2
-    .then((topics) => {
+    .then((topics) => {    //if the return  Topic.findAll is successful then, the .then part is executed.
       callback(null, topics);
     })
-    .catch((err) => {
+    .catch((err) => {   //if the return Topic.findAll is not successful, then the .catch(err) part is executed.
       callback(err);
     })
   },
