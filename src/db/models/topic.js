147ffee4fec9f0cbd631253c7010cf0636553fb5
Renuka <renukaalurkar@gmail.com>
2019-12-07 21:49:00 -0600

    create topic null validations

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,15 +1,26 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+  //  title: DataTypes.STRING,
+  title:{
+       type: DataTypes.STRING,
+       allowNull: false
+  },
+    description:{
+      type: DataTypes.STRING,
+       allowNull: false
+    }
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
      foreignKey: "topicId",
-     as: "banners",
-   });
+     as: "banners"
+   }),
+   Topic.hasMany(models.Post, {
+       foreignKey: "topicId",
+       as: "posts"
+     });
   };
   return Topic;
 };
