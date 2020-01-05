c88417d680f0173d5889e87f3eada7ef3a9ff31a
Renuka <renukaalurkar@gmail.com>
2020-01-05 17:18:53 -0600

    resolving error

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -6,19 +6,19 @@ module.exports = (sequelize, DataTypes) => {
      allowNull: false,
      validate: {
        isEmail: { msg: "must be a valid email" }
-     }
-   },
+     }//validate
+   },//email
 
    password: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
       isEmail: { msg: "must be a valid email" }
-    }
-  },
-  }, {});
+    }//validate
+  },//password
+}, {});//var User
   User.associate = function(models) {
     // associations can be defined here
-  };
+  };//user.associate
   return User;
-};
+};//module exports
