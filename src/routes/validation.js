74d009371214b286f8a636c88aa2bc2c53504c3e
Renuka <renukaalurkar@gmail.com>
2020-01-09 20:46:26 -0600

    fixing errors

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -30,7 +30,7 @@ module.exports = {
     if(req.method === "POST") {
 
 //#2
-      req.checkParams("id", "must be valid").notEmpty().isInt();
+    //  req.checkParams("id", "must be valid").notEmpty().isInt();
       req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
       req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
     }
