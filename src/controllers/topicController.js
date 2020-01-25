a04f3dda478e0c26d810fd3bbdbbd117e32108d4
Renuka <renukaalurkar@gmail.com>
2020-01-24 20:50:58 -0600

    working on checkpoint

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,4 +1,5 @@
 const topicQueries = require("../db/queries.topics.js");
+const Authorizer = require("../policies/topic");
 module.exports = {
   index(req, res, next){ //creating a function called index
   //  res.send("TODO: list all topics");
@@ -14,10 +15,23 @@ module.exports = {
       })
   },
 
-new(req,res,next){
-  res.render("topics/new");
-},
+//new(req,res,next){
+//  res.render("topics/new");
+//},
 
+new(req, res, next){
+ // #2
+     const authorized = new Authorizer(req.user).new();
+
+     if(authorized) {
+       res.render("topics/new");
+     } else {
+       req.flash("notice", "You are not authorized to do that.");
+       res.redirect("/topics");
+     }
+   },
+
+/*
 create(req, res, next){
    let newTopic = {
      title: req.body.title,
@@ -30,7 +44,33 @@ create(req, res, next){
        res.redirect(303, `/topics/${topic.id}`);
      }
    });
- },
+ },*/
+
+ create(req, res, next){
+
+ // #1
+     const authorized = new Authorizer(req.user).create();
+
+ // #2
+     if(authorized) {
+       let newTopic = {
+         title: req.body.title,
+         description: req.body.description
+       };
+       topicQueries.addTopic(newTopic, (err, topic) => {
+         if(err){
+           res.redirect(500, "topics/new");
+         } else {
+           res.redirect(303, `/topics/${topic.id}`);
+         }
+       });
+     } else {
+
+ // #3
+       req.flash("notice", "You are not authorized to do that.");
+       res.redirect("/topics");
+     }
+   },
 
  show(req,res,next){
   topicQueries.getTopic(req.params.id,(err,topic)=> {
@@ -44,7 +84,7 @@ create(req, res, next){
   });//getTopic close
 },//show close
 
- destroy(req, res, next){
+/* destroy(req, res, next){
       topicQueries.deleteTopic(req.params.id, (err, topic) => {
         if(err){
           res.redirect(500, `/topics/${topic.id}`)
@@ -52,9 +92,20 @@ create(req, res, next){
           res.redirect(303, "/topics")
         }
       });
-    },
+    },*/
 
-    edit(req, res, next){
+    destroy(req, res, next){
+// #1
+     topicQueries.deleteTopic(req, (err, topic) => {
+       if(err){
+         res.redirect(err, `/topics/${req.params.id}`)
+       } else {
+         res.redirect(303, "/topics")
+       }
+     });
+   },
+
+  /*  edit(req, res, next){
     topicQueries.getTopic(req.params.id, (err, topic) => {
       if(err || topic == null){
         res.redirect(404, "/");
@@ -62,7 +113,28 @@ create(req, res, next){
         res.render("topics/edit", {topic});
       }
     });
-  },
+  },*/
+
+  edit(req, res, next){
+// #1
+     topicQueries.getTopic(req.params.id, (err, topic) => {
+       if(err || topic == null){
+         res.redirect(404, "/");
+       } else {
+
+ // #2
+         const authorized = new Authorizer(req.user, topic).edit();
+
+ // #3
+         if(authorized){
+           res.render("topics/edit", {topic});
+         } else {
+           req.flash("You are not authorized to do that.")
+           res.redirect(`/topics/${req.params.id}`)
+         }
+       }
+     });
+   },
 
   update(req, res, next){
 //#1
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -66,6 +66,7 @@ addTopic(newTopic, callback){
       })
     },
 
+/*
     updateTopic(id, updatedTopic, callback){
     return Topic.findByPk(id)
     .then((topic) => {
@@ -84,6 +85,41 @@ addTopic(newTopic, callback){
         callback(err);
       });
     });
-  }
+  }*/
+
+  updateTopic(req, updatedTopic, callback){
+
+// #1
+     return Topic.findByPk(req.params.id)
+     .then((topic) => {
+
+// #2
+       if(!topic){
+         return callback("Topic not found");
+       }
+
+// #3
+       const authorized = new Authorizer(req.user, topic).update();
+
+       if(authorized) {
+
+// #4
+         topic.update(updatedTopic, {
+           fields: Object.keys(updatedTopic)
+         })
+         .then(() => {
+           callback(null, topic);
+         })
+         .catch((err) => {
+           callback(err);
+         });
+       } else {
+
+// #5
+         req.flash("notice", "You are not authorized to do that.");
+         callback("Forbidden");
+       }
+     });
+   }
 
 }//module.exports close
